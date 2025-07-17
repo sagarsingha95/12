9fa282e8546f14061c658c1362093185f6d9727d
Sagar Singha <sagarsingha619@gmail.com>
2025-07-17 17:45:31 +0530

    stage2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import './index.css'
+// import App from './App.jsx'
+import StarRating from '../starRating'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} messages ={["terrible","bad","Average","good","Amazing"]}/>
+    <StarRating maxRating={5} color='red'/>
   </StrictMode>,
 )
